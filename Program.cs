9f41c5f6ee7f554b101b92e36f03d4f669eb6803
MustoLini <johannes.danielsson@student.forsbergsskola.se>
2021-11-11 13:50:47 +0100

    Fix on the leachtur

--- Program.cs
+++ Program.cs
@@ -7,15 +7,28 @@ namespace SharpEngine
 {
     class Program
     {
-        static Vector[] vertices = new Vector[] {
-            new Vector(-.1f, -.1f),
-            new Vector(.1f, -.1f),
-            new Vector(0f, .1f),
-            
-            new Vector(.4f, .4f),
-            new Vector(.6f, .4f),
-            new Vector(.5f, .6f),
+        public struct Vertex {
+            public Vector position;
 
+            public Vertex(Vector position) {
+                this.position = position;
+            }
+        }
+
+        // static Vector[] vertices = new Vector[] {
+        //     new Vector(-.1f, -.1f),
+        //     new Vector(.1f, -.1f),
+        //     new Vector(0f, .1f),
+        //     
+        //     new Vector(.4f, .4f),
+        //     new Vector(.6f, .4f),
+        //     new Vector(.5f, .6f),
+        //
+        // };
+        static Vertex[] vertices = new Vertex[] {
+            new Vertex(new Vector(0f, 0f)),
+            new Vertex(new Vector(1f, 0f)),
+            new Vertex(new Vector(0f, 1f))
         };
 
         private static bool test;
@@ -46,27 +59,27 @@ namespace SharpEngine
                 // 1.1 Move the Triangle to the Center, so we can scale it without Side Effects
                 // 1.1.1 Find the Center of the Triangle
                 // 1.1.1.1 Find the Minimum and Maximum
-                var min = vertices[0];
+                var min = vertices[0].position;
                 for (var i = 1; i < vertices.Length; i++) {
-                    min = Vector.Min(min, vertices[i]);
+                    min = Vector.Min(min, vertices[i].position);
                 }
-                var max = vertices[0];
+                var max = vertices[0].position;
                 for (var i = 1; i < vertices.Length; i++) {
-                    max = Vector.Max(max, vertices[i]);
+                    max = Vector.Max(max, vertices[i].position);
                 }
                 // 1.1.1.2 Average out the Minimum and Maximum to get the Center
                 var center = (min + max) / 2;
                 // 1.1.2 Move the Triangle the Center
                 for (var i = 0; i < vertices.Length; i++) {
-                    vertices[i] -= center;
+                    vertices[i].position -= center;
                 }
                 // 1.2 Scale the Triangle
                 for (var i = 0; i < vertices.Length; i++) {
-                    vertices[i] *= multip;
+                    vertices[i].position *= multip;
                 }
                 // 1.3 Move the Triangle Back to where it was before
                 for (var i = 0; i < vertices.Length; i++) {
-                    vertices[i] += center;
+                    vertices[i].position += center;
                 }
                 
                 // 2. Keep track of the Scale, so we can reverse it
@@ -79,11 +92,11 @@ namespace SharpEngine
                 }
                 
                 for (var i = 0; i < vertices.Length; i++) {
-                    vertices[i] += direction;
+                    vertices[i].position += direction;
                 }
                 // 4. Check the X-Bounds of the Screen
                 for (var i = 0; i < vertices.Length; i++) {
-                    if (vertices[i].x >= 1 && direction.x > 0 || vertices[i].x <= -1 && direction.x < 0) {
+                    if (vertices[i].position.x>= 1 && direction.x > 0 || vertices[i].position.x <= -1 && direction.x < 0) {
                         direction.x *= -1;
                         break;
                     }
@@ -91,7 +104,7 @@ namespace SharpEngine
 
                 for (var i = 0; i < vertices.Length; i++)
                 {
-                    if (vertices[i].y >= 1 && direction.y > 0 || vertices[i].y <= -1 && direction.y < 0)
+                    if (vertices[i].position.y >= 1 && direction.y > 0 || vertices[i].position.y <= -1 && direction.y < 0)
                     {
                         direction.y *= -1;
                         break;
@@ -165,8 +178,8 @@ namespace SharpEngine
 
         }
         static unsafe void UpdateTriangleBuffer() {
-            fixed (Vector* vertex = &vertices[0]) {
-                glBufferData(GL_ARRAY_BUFFER, sizeof(Vector) * vertices.Length, vertex, GL_STATIC_DRAW);
+            fixed (Vertex* vertex = &vertices[0]) {
+                glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.Length, vertex, GL_STATIC_DRAW);
             }
         }
        
